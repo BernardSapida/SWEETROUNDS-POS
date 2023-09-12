1f69a92e2b014cddb6cef8110ddd8d1d643ed87e
Bernard Sapida <BERNARDSAPIDA1706@GMAIL.COM>
2023-09-12 19:32:18 +0800

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,29 @@
-function Index() {
-    return (<h1>Hello World</h1>);
-}
+import SignInForm from "@/components/signin/Form";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import { getSession } from "next-auth/react";
 
-export default Index;
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const { req } = context;
+  const session = await getSession({ req: req });
+
+  if (session) {
+    return {
+      props: {},
+      redirect: {
+        destination: "/admin/dashboard",
+      },
+    };
+  }
+
+  return { props: {} };
+};
+
+export default function Home() {
+  return (
+    <div className="d-flex align-items-center" style={{ height: "80vh" }}>
+      <SignInForm />
+    </div>
+  );
+}

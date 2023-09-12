c32a1f1290a61c6a202fb05ef5f05af35bccc9bb
Bernard Sapida <BERNARDSAPIDA1706@GMAIL.COM>
2023-09-12 18:59:18 +0800

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,7 +30,7 @@ export default function App({
           content="Bernard Sapida, Shania Gwyneth Nuga, Ralph Howard Azarcon"
         />
         <meta property="og:title" content="Sweet Rounds" />
-        <meta property="og:url" content="https://sweetrounds-pos.vercel.app/" />
+        <meta property="og:url" content="https://sweetroundspos.vercel.app/" />
         <meta
           property="og:image"
           content="https://images.pexels.com/photos/3628508/pexels-photo-3628508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
